c9b70def723b4717c9c263f40f0161abf3ac5f02
prokushev <yuri.prokushev@gmail.com>
2018-10-09 22:03:37 +0700

    GPL2->GPL3 as possible by original license

--- cpmutl.h
+++ cpmutl.h
@@ -1,11 +1,13 @@
 /**************************************************************************
 
-    This file is part of the CP/NET 1.1/1.2 server emulator for Unix.
-    Copyright (C) 2005, Hector Peraza.
+    This file is part of the CP/NET 1.1/1.2 server emulator for Win32
+    systems. Copyright (C) 2004,2005, Hector Peraza.
+    Portions copyright (C) 2005-2017. Teunis van Beelen (teuniz@gmail.com)
+    Portions copyright (C) 2018, Yuri Prokushev
 
-    This program is free software; you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -14,8 +16,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 **************************************************************************/
 
--- rs232.c
+++ rs232.c
@@ -1,29 +1,25 @@
-/*
-***************************************************************************
-*
-* Author: Teunis van Beelen
-*
-* Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 Teunis van Beelen
-*
-* Email: teuniz@gmail.com
-*
-***************************************************************************
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-***************************************************************************
-*/
+/*************************************************************************
+
+    This file is part of the CP/NET 1.1/1.2 server emulator for Win32
+    systems. Copyright (C) 2004,2005, Hector Peraza.
+    Portions copyright (C) 2005-2017. Teunis van Beelen (teuniz@gmail.com)
+    Portions copyright (C) 2018, Yuri Prokushev
+
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************/
 
 
 /* Last revision: November 22, 2017 */
